3b267566cb8b9136f993dcb2db2282ba8e2eca1c
Josh Humpherys <joshua.humpherys@gmail.com>
2016-07-31 14:40:45 -0400

    Add settings for color picker

--- app/src/main/java/com/android/cubemaster/colors/MainActivity.java
+++ app/src/main/java/com/android/cubemaster/colors/MainActivity.java
@@ -1,6 +1,9 @@
 package com.android.cubemaster.colors;
 
 import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -9,9 +12,11 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.support.annotation.ColorInt;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Display;
 import android.view.View;
@@ -25,8 +30,13 @@ import com.pavelsikun.vintagechroma.OnColorSelectedListener;
 import com.pavelsikun.vintagechroma.colormode.ColorMode;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Map<String, ColorMode> mColorModeMap;
+    private Map<String, IndicatorMode> mIndicatorModeMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +45,40 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mColorModeMap = new HashMap<>();
+        mColorModeMap.put(getString(R.string.pref_color_mode_argb), ColorMode.ARGB);
+        mColorModeMap.put(getString(R.string.pref_color_mode_rgb), ColorMode.RGB);
+        mColorModeMap.put(getString(R.string.pref_color_mode_cmyk), ColorMode.CMYK);
+        mColorModeMap.put(getString(R.string.pref_color_mode_cmyk255), ColorMode.CMYK255);
+        mColorModeMap.put(getString(R.string.pref_color_mode_hsl), ColorMode.HSL);
+        mColorModeMap.put(getString(R.string.pref_color_mode_hsv), ColorMode.HSV);
+
+        mIndicatorModeMap = new HashMap<>();
+        mIndicatorModeMap.put(getString(R.string.pref_indicator_mode_hex), IndicatorMode.HEX);
+        mIndicatorModeMap.put(getString(R.string.pref_indicator_mode_decimal), IndicatorMode.DECIMAL);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+                String colorModePreferenceKey = getString(R.string.pref_color_mode_key);
+                String indicatorModePreferenceKey = getString(R.string.pref_indicator_mode_key);
+
+                String colorModePreferenceDefault = getString(R.string.pref_color_mode_default);
+                String indicatorModePreferenceDefault = getString(R.string.pref_indicator_mode_default);
+
+                String colorModeString = sharedPreferences.getString(colorModePreferenceKey, colorModePreferenceDefault);
+                String indicatorModeString = sharedPreferences.getString(indicatorModePreferenceKey, indicatorModePreferenceDefault);
+
+                ColorMode colorMode = mColorModeMap.get(colorModeString);
+                IndicatorMode indicatorMode = mIndicatorModeMap.get(indicatorModeString);
+
                 new ChromaDialog.Builder()
                         .initialColor(Color.GREEN)
-                        .colorMode(ColorMode.RGB) // RGB, ARGB, HVS, CMYK, CMYK255, HSL
-                        .indicatorMode(IndicatorMode.HEX) //HEX or DECIMAL; Note that (HSV || HSL || CMYK) && IndicatorMode.HEX is a bad idea
+                        .colorMode(colorMode)
+                        .indicatorMode(indicatorMode)
                         .onColorSelected(new OnColorSelectedListener() {
                             @Override
                             public void onColorSelected(@ColorInt int color) {
@@ -102,8 +138,8 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
---
+++ app/src/main/java/com/android/cubemaster/colors/SettingsActivity.java
@@ -0,0 +1,58 @@
+package com.android.cubemaster.colors;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.pref_general);
+
+        Preference colorModePreference = findPreference(getString(R.string.pref_color_mode_key));
+        Preference indicatorModePreference = findPreference(getString(R.string.pref_indicator_mode_key));
+
+        bindPreferenceSummaryToValue(colorModePreference);
+        bindPreferenceSummaryToValue(indicatorModePreference);
+    }
+
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(this);
+
+        onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String stringValue = value.toString();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            preference.setSummary(stringValue);
+        }
+
+        return true;
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public Intent getParentActivityIntent() {
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    }
+}
